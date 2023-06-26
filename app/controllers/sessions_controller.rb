5bc7febfe1dd27cbd5259d4c245272835ba1053c
MarsMwau <marthamumbua16@gmail.com>
2023-06-27 00:12:26 +0300

    Completef Lab

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,8 @@
 class SessionsController < ApplicationController
   
   def index
-    session[:session_hello] ||= "World"
-    cookies[:cookies_hello] ||= "World"
+    session[:session_hello] = "Hello, Rails" if session[:session_hello].nil?
+    cookies[:cookies_hello] = "Hello, Cookies" if cookies[:cookies_hello].nil?
     render json: { session: session, cookies: cookies.to_hash }
   end
-
 end
